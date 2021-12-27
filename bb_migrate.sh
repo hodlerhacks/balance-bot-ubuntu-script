0150b2faf06f6519be471efd877e5fa0ba943faf
hodlerhacks <leon@hodlerhacks.com>
2021-12-27 10:39:54 +0100

    Include Balance Bot migration script

--- bb_migrate.sh
+++ bb_migrate.sh
@@ -24,6 +24,7 @@ migrate_bot() {
             echo "It seems that migration has already been performed"
         else
             echo "It seems that Balance Bot is not properly installed or setup"
+        fi
 	fi
 
 	rm -r "$BBPATH"/"$BBFOLDER"
